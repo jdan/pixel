3ab2f71b3db4ef0806621d85822f638b843d5285
Jordan Scales <scalesjordan@gmail.com>
2020-10-27 14:25:14 -0400

    put useArtiste in react

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,42 @@
 import Head from "next/head";
-import { useRef } from "react";
-import { useArtiste } from "../src/artiste";
+import { useRef, useState, useEffect } from "react";
+import * as shaders from "../src/shaders";
+import makeArtiste from "../src/artiste";
+
+const FPS = 60;
+
+function useArtiste(canvas) {
+  let [time, setTime] = useState(0);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      if (canvas.current) {
+        let ctx = canvas.current.getContext("2d");
+        let imageData = ctx.createImageData(32, 32);
+
+        let art = makeArtiste({
+          width: 32,
+          height: 32,
+          drawPixel: (x, y, [r, g, b]) => {
+            imageData.data[32 * 4 * y + x * 4] = r;
+            imageData.data[32 * 4 * y + x * 4 + 1] = g;
+            imageData.data[32 * 4 * y + x * 4 + 2] = b;
+            imageData.data[32 * 4 * y + x * 4 + 3] = 255;
+          },
+          onFinish: () => {
+            ctx.putImageData(imageData, 0, 0);
+          },
+        });
+
+        art.drawPixelShader(shaders.gradient);
+      }
+
+      setTime((time) => time + 1);
+    }, 1000 / FPS);
+
+    return () => clearTimeout(timer);
+  }, [time]);
+}
 
 export default function Home() {
   let canvas = useRef(null);
--- src/artiste.js
+++ src/artiste.js
@@ -1,8 +1,3 @@
-let { useState, useEffect } = require("react");
-let shaders = require("./shaders");
-
-const FPS = 60;
-
 module.exports = function makeArtiste({ width, height, drawPixel, onFinish }) {
   return {
     drawPixelShader(shader) {
@@ -16,37 +11,4 @@ module.exports = function makeArtiste({ width, height, drawPixel, onFinish }) {
       onFinish();
     },
   };
-};
-
-exports.useArtiste = (canvas) => {
-  let [time, setTime] = useState(0);
-
-  useEffect(() => {
-    const timer = setTimeout(() => {
-      if (canvas.current) {
-        let ctx = canvas.current.getContext("2d");
-        let imageData = ctx.createImageData(32, 32);
-
-        let art = makeArtiste({
-          width: 32,
-          height: 32,
-          drawPixel: (x, y, [r, g, b]) => {
-            imageData.data[32 * 4 * y + x * 4] = r;
-            imageData.data[32 * 4 * y + x * 4 + 1] = g;
-            imageData.data[32 * 4 * y + x * 4 + 2] = b;
-            imageData.data[32 * 4 * y + x * 4 + 3] = 255;
-          },
-          onFinish: () => {
-            ctx.putImageData(imageData, 0, 0);
-          },
-        });
-
-        art.drawPixelShader(shaders.mandel);
-      }
-
-      setTime((time) => time + 1);
-    }, 1000 / FPS);
-
-    return () => clearTimeout(timer);
-  }, [time]);
 };
