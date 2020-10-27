3a412e00dcd8fb05bf2ee655ae7e0b7c59ecb3d7
Jordan Scales <scalesjordan@gmail.com>
2020-10-27 12:26:23 -0400

    use imageData instead of fillRect

--- src/artiste.js
+++ src/artiste.js
@@ -1,31 +1,33 @@
 import { useState, useEffect } from "react";
 
-const FPS = 30;
+const FPS = 60;
 
 const shader = (x, y) => {
   let t = new Date() / 1000;
-  return 2 > Math.abs(x - Math.floor(Math.sin(t) * 16 + 16)) &&
-    2 > Math.abs(y - Math.floor(Math.cos(t) * 16 + 16))
+  return x === Math.floor(Math.sin(t * 2) * 16 + 16) &&
+    y === Math.floor(Math.cos(t * 2) * 16 + 16)
     ? [255, 255, 255]
     : [0, 0, 0];
 };
 
 export function useArtiste() {
-  let pixels = Array.from({ length: 32 * 32 });
   let [time, setTime] = useState(0);
 
   useEffect(() => {
     const timer = setTimeout(() => {
-      const ctx = document.getElementById("canvas").getContext("2d");
+      let ctx = document.getElementById("canvas").getContext("2d");
 
+      let imageData = ctx.createImageData(32, 32);
       for (let x = 0; x < 32; x++) {
         for (let y = 0; y < 32; y++) {
-          let rgb = shader(x, y);
-          pixels[32 * y + x] = rgb;
-          ctx.fillStyle = `rgb(${rgb.join(", ")})`;
-          ctx.fillRect(x, y, 1, 1);
+          let [r, g, b] = shader(x, y);
+          imageData.data[32 * 4 * y + x * 4] = r;
+          imageData.data[32 * 4 * y + x * 4 + 1] = g;
+          imageData.data[32 * 4 * y + x * 4 + 2] = b;
+          imageData.data[32 * 4 * y + x * 4 + 3] = 255;
         }
       }
+      ctx.putImageData(imageData, 0, 0);
 
       setTime((time) => time + 1);
     }, 1000 / FPS);
