c58e70c9d8ab34f4fd2a7c803c6e6adc71398455
Jordan Scales <scalesjordan@gmail.com>
2020-10-27 12:44:51 -0400

    gradient shader

--- src/artiste.js
+++ src/artiste.js
@@ -2,13 +2,19 @@ import { useState, useEffect } from "react";
 
 const FPS = 60;
 
-const shader = (x, y) => {
+const dot = (x, y) => {
   let t = new Date() / 1000;
   return x === Math.floor(Math.sin(t * 2) * 16 + 16) &&
     y === Math.floor(Math.cos(t * 2) * 16 + 16)
     ? [255, 255, 255]
     : [0, 0, 0];
 };
+
+const gradient = (x, y) => {
+  return [Math.floor((255 * x) / 32), 0, Math.floor((255 * y) / 32)];
+};
+
+let shader = gradient;
 
 export function useArtiste(canvas) {
   let [time, setTime] = useState(0);
@@ -22,6 +28,11 @@ export function useArtiste(canvas) {
         for (let x = 0; x < 32; x++) {
           for (let y = 0; y < 32; y++) {
             let [r, g, b] = shader(x, y);
+
+            // artiste should take an x, y, r, g, and b and either
+            // - matrix.setPixel()
+            // - imageData.data[...] = ...
+
             imageData.data[32 * 4 * y + x * 4] = r;
             imageData.data[32 * 4 * y + x * 4 + 1] = g;
             imageData.data[32 * 4 * y + x * 4 + 2] = b;
